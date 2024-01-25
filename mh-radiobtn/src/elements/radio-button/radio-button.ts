3f61312d71374be93cd983779787b4765cbb7b11
Paul <paul.mcneiljr@gmail.com>
2024-01-25 13:54:26 -0500

    initial radio-btn

---
+++ mh-radiobtn/src/elements/radio-button/radio-button.ts
@@ -0,0 +1,20 @@
+import { bindable, bindingMode, TaskQueue, autoinject } from 'aurelia-framework';
+
+@autoinject
+export class RadioButton {
+    @bindable({defaultBindingMode: bindingMode.twoWay}) private value: any;
+    @bindable private model: any;
+    @bindable private label: string;
+    @bindable private name: string;
+    @bindable private autoFocus: boolean = false;
+    private focus: boolean = false;
+    private id = Math.random();
+
+    constructor(private taskQueue: TaskQueue) { }
+
+    attached() {
+        if(this.autoFocus) {
+            this.taskQueue.queueTask(() => this.focus=true)
+        }
+    }
+}
